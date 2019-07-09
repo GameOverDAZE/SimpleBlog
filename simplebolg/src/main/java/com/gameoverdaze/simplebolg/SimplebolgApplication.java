2f2033990b818644c6b327f7d05bd20fdcd5cda4
cxstar.sjw <sjw@cxstar.com>
2019-07-09 16:05:32 +0800

    测试push

---
+++ simplebolg/src/main/java/com/gameoverdaze/simplebolg/SimplebolgApplication.java
@@ -0,0 +1,13 @@
+package com.gameoverdaze.simplebolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebolgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplebolgApplication.class, args);
+    }
+
+}
